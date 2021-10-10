4477547a01685643ecce252b1abec6e8e60649b9
Leandro Sales <leandrossales.96@gmail.com>
2021-10-09 23:29:41 -0300

    add home screen buttons

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,10 +6,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Quiz"),
-          automaticallyImplyLeading: false,
+      appBar: AppBar(
+        title: const Text("Quiz"),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Container(
+                  color: Colors.blue,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 46,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Botão",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Container(
+                  color: Colors.blue,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 46,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Botão",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Container(
+                  color: Colors.blue,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 46,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Botão",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: const Text("To do: Home screen"));
+      ),
+    );
   }
 }
